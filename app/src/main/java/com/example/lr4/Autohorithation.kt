a29d00d9230d0022af68b82623ecd7d4b72a4a9c
Foxeeek <43350990+Foxeeek@users.noreply.github.com>
2021-05-28 01:24:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lr4/Autohorithation.kt
@@ -0,0 +1,11 @@
+package com.example.lr4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Autohorithation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_autohorithation)
+    }
+}
